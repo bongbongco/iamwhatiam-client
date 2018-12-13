d451c2ba96c160de7171a88b000b482106703670
seungyonglee <bongbongco@gmail.com>
2018-12-14 06:52:38 +0900

    Edit PhoneLogin

--- iamwhatiam-client/src/Routes/PhoneLogin/PhoneLoginContainer.tsx
+++ iamwhatiam-client/src/Routes/PhoneLogin/PhoneLoginContainer.tsx
@@ -1,9 +1,51 @@
 import React from "react";
+import { RouteComponentProps } from "react-router-dom";
+import { toast } from "react-toastify";
 import PhoneLoginPresenter from "./PhoneLoginPresenter";
 
-class PhoneLoginContainer extends React.Component {
+interface IState {
+    countryCode: string;
+    phoneNumber: string;
+}
+
+class PhoneLoginContainer extends React.Component<
+RouteComponentProps<any>,
+IState
+> {
+    public state = {
+        countryCode: "+82",
+        phoneNumber: ""
+    }
+
     public render() {
-        return <PhoneLoginPresenter />;
+        const {countryCode, phoneNumber} = this.state;
+        return (
+            <PhoneLoginPresenter 
+                countryCode={countryCode} 
+                phoneNumber={phoneNumber} 
+                onInputChange={this.onInputChange}
+                onSubmit={this.onSubmit}
+            />
+        );
+    }
+
+    public onInputChange: React.ChangeEventHandler<
+        HTMLInputElement | HTMLSelectElement
+    > = event => {
+        const {
+            target: { name, value }
+        } = event;
+        this.setState({
+            [name]: value
+        } as any);
+    }
+
+    public onSubmit: React.FormEventHandler<HTMLFormElement> = event => {
+        event.preventDefault();
+        const { countryCode, phoneNumber } = this.state;
+        // tslint:disable-next-line
+        console.log(countryCode, phoneNumber);
+        toast.info("Info Notification!");
     }
 }
 
--- iamwhatiam-client/src/Routes/PhoneLogin/PhoneLoginPresenter.tsx
+++ iamwhatiam-client/src/Routes/PhoneLogin/PhoneLoginPresenter.tsx
@@ -53,23 +53,44 @@ const Button = styled.button`
     cursor: pointer;
 `;
 
-const PhoneLoginPresenter = () => (
+interface IProps {
+    countryCode: string;
+    phoneNumber: string;
+    onInputChange: (
+        event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+    ) => void;
+    onSubmit: (
+        event: React.FormEvent<HTMLFormElement>
+    ) => void;
+}
+
+const PhoneLoginPresenter: React.SFC<IProps> = ({
+    countryCode,
+    phoneNumber,
+    onInputChange,
+    onSubmit
+}) => (
     <Container>
         <Helmet>
             <title>Phone Login | IamwhatIam</title>
         </Helmet>
         <BackArrowExtended backTo={"/"} />
         <Title>휴대전화 번호를 입력해주세요</Title>
-        <CountrySelect>
+        <CountrySelect value={countryCode} name={"countryCode"} onChange={onInputChange}>
             {countries.map((country, index) => (
                 <CountryOption key={index} value={country.dial_code}>
                     {country.flag} {country.name} ({country.dial_code})
                 </CountryOption>
             ))}
         </CountrySelect>
-        <Form>
-            <Input placeholder={"053 690 2129"} />
-            <Button>
+        <Form onSubmit={onSubmit}>
+            <Input 
+                placeholder={"053 690 2129"} 
+                value={phoneNumber} 
+                name={"phoneNumber"}
+                onChange={onInputChange}
+            />
+            <Button onClick={onSubmit}>
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="24"
