8bf0c3c9cf351d46c02baaa8be0606065d9ff813
seungyonglee <bongbongco@gmail.com>
2018-12-17 05:28:58 +0900

    Add apollo codegen

--- iamwhatiam-client/src/Routes/PhoneLogin/PhoneLoginContainer.tsx
+++ iamwhatiam-client/src/Routes/PhoneLogin/PhoneLoginContainer.tsx
@@ -1,16 +1,27 @@
 import React from "react";
+import { Mutation } from "react-apollo";
 import { RouteComponentProps } from "react-router-dom";
 import { toast } from "react-toastify";
 import PhoneLoginPresenter from "./PhoneLoginPresenter";
+import { PHONE_SIGN_IN } from "./PhoneQueries.queries";
+
 
 interface IState {
     countryCode: string;
     phoneNumber: string;
 }
 
+interface IMutationInterface {
+    phoneNumber: string;
+}
+
+class PhoneSignInMutation extends Mutation<any, 
+    IMutationInterface
+> {}
+
 class PhoneLoginContainer extends React.Component<
-RouteComponentProps<any>,
-IState
+    RouteComponentProps<any>,
+    IState
 > {
     public state = {
         countryCode: "+82",
@@ -20,12 +31,36 @@ IState
     public render() {
         const {countryCode, phoneNumber} = this.state;
         return (
-            <PhoneLoginPresenter 
-                countryCode={countryCode} 
-                phoneNumber={phoneNumber} 
-                onInputChange={this.onInputChange}
-                onSubmit={this.onSubmit}
-            />
+            <PhoneSignInMutation
+                mutation={PHONE_SIGN_IN}
+                variables={{
+                    phoneNumber: `${countryCode}${phoneNumber}` 
+                }}
+            >
+                {(mutation, {loading}) => {
+                    const onSubmit: React.FormEventHandler<HTMLFormElement> = event => {
+                        event.preventDefault();
+                        const isValid = /^\+[1-9]{1}[0-9]{7,11}$/.test(
+                            `${countryCode}${phoneNumber}`
+                        );
+                        if (isValid) {
+                            mutation();
+                            return;
+                        } else {
+                            toast.error("전화번호를 다시 확인해주세요");
+                        }
+                    }
+                    return (
+                        <PhoneLoginPresenter 
+                            countryCode={countryCode} 
+                            phoneNumber={phoneNumber} 
+                            onInputChange={this.onInputChange}
+                            onSubmit={onSubmit}
+                            loading={loading}
+                        />
+                    )
+                }}
+            </PhoneSignInMutation>
         );
     }
 
@@ -38,14 +73,6 @@ IState
         this.setState({
             [name]: value
         } as any);
-    }
-
-    public onSubmit: React.FormEventHandler<HTMLFormElement> = event => {
-        event.preventDefault();
-        const { countryCode, phoneNumber } = this.state;
-        // tslint:disable-next-line
-        console.log(countryCode, phoneNumber);
-        toast.info("Info Notification!");
     }
 }
 
